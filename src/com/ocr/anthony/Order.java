a195cbbba458311bd5ca0e30661ba6b1c332d4ce
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2018-09-20 11:33:03 +0200

    Add behaviour in order : - method displaySelectedMenu with conditions

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -6,9 +6,9 @@ public class Order {
      */
     public void displayAvailableMenu() {
         System.out.println("Choix du menu");
-        System.out.println("1 - Poulet");
-        System.out.println("2 - Boeuf");
-        System.out.println("3 - Vegetarien");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
         System.out.println("Que souhaitez-vous comme menu ?");
     }
     /**
@@ -16,6 +16,14 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-        System.out.println("Vous avez choisi le menu " + nbMenu);
+        if (nbMenu == 1) {
+            System.out.println("Vous avez choisi comme menu : poulet");
+        } else if (nbMenu == 2) {
+            System.out.println("Vous avez choisi comme menu : boeuf");
+        } else if (nbMenu == 3) {
+            System.out.println("Vous avez choisi comme menu : végétarien");
+        } else {
+            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+        }
     }
 }
